1ae28b9bba1685dae383568274bb0684455cb36e
jarnalyrkar <joachimsolstrom@gmail.com>
2018-05-26 11:55:20 +0200

    first commit

---
+++ ex3.rb
@@ -0,0 +1,42 @@
+# Prints a text string
+puts "I will now count my chickens:"
+
+# Prints "Hens", and then does
+# the calculation 25.0 + 30.0 / 6.0
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Prints "Roosters", and then does
+# the calculation 100 - 25 * 3 % 4
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Prints a text string
+puts "Now I will count the eggs:"
+
+# Prints the answer to the calculation below
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Prints the string below, literally; no calculation
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints true or false, depending on the condition below 
+# (returns false; 5 is not less than negative 2)
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Prints both the string, and the answer
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Prints a text string
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# Prints a string, and then checks if the condition
+# is true or false.
+
+# true
+puts "Is it greater? #{5.0 > -2.0}"
+# true
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# false
+puts "Is it less or equal? #{5.0 <= -2.0}"
+
---
+++ ex3b.rb
@@ -0,0 +1,2 @@
+puts "My calculation looks like this: 9 + (3 * 2)"
+puts "which gives me the answer #{9 + (3 * 2)}" 
