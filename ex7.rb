bacf177fcaf948759169cda703ac2bde1f1239f8
jarnalyrkar <joachimsolstrom@gmail.com>
2018-05-26 12:54:51 +0200

    Ex.7 study drill 1

--- ex7.rb
+++ ex7.rb
@@ -1,8 +1,14 @@
+# Prints the string below
 puts "Mary had a little lamb."
+# Prints the string below, ignoring the characters around
+# the word snow
 puts "Its fleece was white as #{'snow'}."
+# Prints the string below
 puts "And everywhere that Mary went."
+# Prints . ten times.
 puts "." * 10 # what'd that do?
 
+# sets the variable end# to a single character
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +22,8 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+
 # watch that print vs. puts on this line what's it do?
+# puts adds a newline after executing; print does not.
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
