2f78b7519e3c4204fbe67acd9a455f2926d362f8
jarnalyrkar <joachimsolstrom@gmail.com>
2018-05-26 13:05:11 +0200

    Ex.8 study drill 1

--- ex8.rb
+++ ex8.rb
@@ -1,10 +1,14 @@
+# Sets the variable formatter to a string, containing a 
+# a given format of how to output
 formatter = "%{first} %{second} %{third} %{fourth}"
 
+# prints the formatter, and uses the variables set above
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
 puts formatter % {first: true, second: false, third: true, fourth: false}
 puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter }
 
+# Prints the formatter, but this time on multiple lines.
 puts formatter % {
 	first: "I had this thing.",
 	second: "That you could type up right.",
