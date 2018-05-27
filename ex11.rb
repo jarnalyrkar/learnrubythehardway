5aa1c67a0547a096fbf6581d576318c6f4f30526
jarnalyrkar <joachimsolstrom@gmail.com>
2018-05-27 16:07:21 +0200

    Ex. 11

---
+++ ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+# gets waits for input, chomps cuts off any newline
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh ?"
+weight = gets.chomp
+
+puts "So, you're #{age}, #{height} tall and #{weight} heavy."
+
+print "What dnd class is your favorite? "
+dnd_class = gets.chomp
+print "What dnd race is your favorite? "
+dnd_race = gets.chomp
+
+
+
+
