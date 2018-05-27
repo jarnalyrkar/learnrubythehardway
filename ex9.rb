15d262103fdc70203d8dd8217fe718cda6fe754e
jarnalyrkar <joachimsolstrom@gmail.com>
2018-05-27 09:29:05 +0200

    Ex.9, study drill 1

--- ex9.rb
+++ ex9.rb
@@ -1,11 +1,18 @@
 # Here's some new strange stuff, remember type it exactly.
 
+# Sets the variable days to the text string below
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# Sets the variable months to the text string below.
+# the \n adds a newline.
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# Prints the content of the variable days
 puts "Here are the days: #{days}"
+# Prints the content of the variable months
 puts "Here are the months: #{months}"
 
+# Prints 
+# %q is a substitute for single quotes
 puts %q{
 	There's something going on here.
 	With this weird quote
